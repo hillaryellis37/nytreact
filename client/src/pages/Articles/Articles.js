76410fc0e3bc5252c8d4df0a2d65047ec18fe5b3
Live Sound Systems <hillaryellis37@gmail.com>
2017-12-03 19:00:25 -0800

    got some components and pages started but currently blank

---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {Row, Col, Container} from "../../components/Main";
+import Jumbotron from "../../components/Jumbotron";
+
+class Articles extends Component {
+
+	render() {
+		return (
+			<Container fluid>
+				<Row>
+					<Col size="md-12">
+					</Col>
+				</Row>
+			</Container>
+
+		);
+	}
+}
+
+export default Articles;
+
+
